43f71d63470fe89f64997b929e270666e42b58f2
sazanjan <sazanjan@student.42heilbronn.de>
2025-12-01 15:28:09 +0000

    Implement libft functions for project - Implemented utility functions for argument validation in main.c. - Added ft_atoi function for string to integer conversion.

---
+++ main.c
@@ -0,0 +1,44 @@
+
+#include "philo.h"
+
+int	check_arg_content(char *arg)
+{
+	int	i;
+
+	i = 0;
+	while (arg[i])
+	{
+		if (arg[i] < '0' || arg[i] > '9')
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+int	check_valid_args(char **argv)
+{
+	if (ft_atoi(argv[1]) > 200 || ft_atoi(argv[1]) <= 0
+		|| check_arg_content(argv[1]) == 1)
+		return (write(2, "Invalid philosophers number\n", 29), 1);
+	if (ft_atoi(argv[2]) <= 0 || check_arg_content(argv[2]) == 1)
+		return (write(2, "Invalid time to die\n", 21), 1);
+	if (ft_atoi(argv[3]) <= 0 || check_arg_content(argv[3]) == 1)
+		return (write(2, "Invalid time to eat\n", 21), 1);
+	if (ft_atoi(argv[4]) <= 0 || check_arg_content(argv[4]) == 1)
+		return (write(2, "Invalid time to sleep\n", 23), 1);
+	if (argv[5] && (ft_atoi(argv[5]) < 0 || check_arg_content(argv[5]) == 1))
+		return (write(2, "Invalid number of times to eat\n", 32), 1);
+	return (0);
+}
+
+int	main(int argc, char **argv)
+{
+
+
+	if (argc != 5 && argc != 6)
+		return (write(2, "Wrong argument count\n", 22), 1);
+	if (check_valid_args(argv) == 1)
+		return (1);
+
+	return (0);
+}
---
+++ utils.c
@@ -0,0 +1,25 @@
+
+int	ft_atoi(const char *s)
+{
+	int		i;
+	int		sign;
+	long	nbr;
+
+	i = 0;
+	nbr = 0;
+	sign = 1;
+	while (s[i] == 32 || (s[i] >= 9 && s[i] <= 13))
+		i++;
+	if (s[i] == '-' || s[i] == '+')
+	{
+		if (s[i] == '-')
+			sign = -1;
+		i++;
+	}
+	while (ft_isdigit(s[i]))
+	{
+		nbr = nbr * 10 + s[i] - 48;
+		i++;
+	}
+	return (nbr * sign);
+}
